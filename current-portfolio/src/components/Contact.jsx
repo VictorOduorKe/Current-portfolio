bb8a4667c81322b3ba07ac6221b63f0a8e676c97
VictorOduorKe <88191160+VictorOduorKe@users.noreply.github.com>
2025-03-22 18:54:23 +0300

    initial commit

---
+++ current-portfolio/src/components/Contact.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <>
+    <section className='contact-container' id='contact'>
+        <h1>CONTACT US HERE</h1>
+        <div className="contact-form">
+            <form action="" id="contact-form">
+               <div className="input-field">
+                <label htmlFor="username">Full name</label>
+                <input type="text" name='username' placeholder='enter full name'/>
+               </div>
+               <div className="input-field">
+                <label htmlFor="phone">Phone No</label>
+                <input type="tel" name='phone' placeholder='enter valid phone number'/>
+               </div>
+               <div className="input-field">
+                <label htmlFor="email">Email</label>
+                <input type="email" name='email' placeholder='enter email'/>
+               </div>
+               <div className="input-field">
+                <label htmlFor="message">Message</label>
+                <textarea name='message' placeholder='type message' id='message'></textarea>
+               </div>
+               <button type='submit'>Send Message</button>
+            </form>
+        </div>
+    </section>
+    </>
+  )
+}
+
+export default Contact
